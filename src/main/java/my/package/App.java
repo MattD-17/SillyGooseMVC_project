b8ced90137dfea33ab514c7047a741a2c3d6fad2
mattd17 <mattdelorme@sasktel.net>
2026-05-03 17:14:24 -0600

    Main app entry point has been created
    The initial MVC setup has been created and now the classes
    just need to be implemented.

---
+++ src/main/java/my/package/App.java
@@ -0,0 +1,34 @@
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application{
+
+
+    public void start(Stage stage){
+        Canvas canvas = new Canvas(800, 600); // create canvas
+
+        // set up MVC connections
+        Model model = new Model();  // holds business logic
+        InteractionModel iModel = new InteractionModel(model);  // interactions with the business logic
+        Controller controller = new Controller(model, InteractionModel);    // decisions of what to do with model and iModel
+        View view = new View(model, iModel, canvas);    // show what the model and iModel represent
+
+        // view delegates event handling to controller
+        view.setupEvents(controller);
+
+        new AnimationTimer(){
+            @Override
+            public void handle(long now){
+                controller.update();
+            }
+        }.start();
+
+        stage.setScene(new Scene(new StackPane(canvas)));
+        stage.show();
+    }
+
+}
